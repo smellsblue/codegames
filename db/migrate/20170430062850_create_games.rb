36adcc121f40876d828cd00a7802ea87770823ed
Mike Virata-Stone <mike@virata-stone.com>
2017-04-29 23:36:48 -0700

    Add initial tables

---
+++ db/migrate/20170430062850_create_games.rb
@@ -0,0 +1,18 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :code, null: false
+      t.boolean :finished, default: false, null: false
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.timestamps
+    end
+
+    create_table :players do |t|
+      t.references :game, foreign_key: true, index: true, null: false
+      t.string :name, null: false
+      t.integer :score, default: 0, null: false
+      t.timestamps
+    end
+  end
+end
